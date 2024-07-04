338a322c7c784aed460406e9328edf095cd61265
Svitlana Popova <diduhdiduh367@gmail.com>
2024-07-04 21:34:00 +0200

    port coregiren backent

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
